0321c00bfe92c867553c720bbae264028beaa0e7
Bianka Rácz <rczbianka1@gmail.com>
2018-10-09 23:45:54 +0200

    Uploading preproCsv.py, entry_data_edited.csv, Deleted_rows. preproCsv.py : Preprocessing the entry_data.csv entry_data_edited.csv: Exported csv after preprocessing Deleted_rows: Number of rows deleted in each phases.

---
+++ src/preproCsv.py
@@ -0,0 +1,32 @@
+import pandas
+
+#Reading the csv file
+df = pandas.read_csv("Data_Entry.csv")
+
+#----------- Remove unnecessary columns -----------
+df.drop(['OriginalImagePixelSpacing_x', 'y', 'OriginalImage_Width', 'Height'], axis = 1, inplace = True)
+
+#----------- Preprocess age column -----------
+
+#Delete rows where age is not in years
+age_filter = df['Patient_Age'].str.contains('[M|D]')
+df = df[~age_filter]
+
+#Convert to integer
+df.loc[:, 'Patient_Age'] = df['Patient_Age'].apply(lambda x: str(x)[:-1]).astype(int)
+
+#Delete rows where age is unrealistic
+df.drop(df[df['Patient_Age'] > 120].index, inplace=True)
+
+
+#----------- Reduce categories -----------
+
+#Delete rows where are more disease
+filter = df['Finding_Labels'].str.contains("\|")
+df = df[~filter]
+
+#Extract dataframe to a csv
+df.to_csv('entry_data_edited.csv')
+#Just for printing out the dataframe
+pandas.set_option("max_columns", None)
+print(df.Finding_Labels.value_counts())
