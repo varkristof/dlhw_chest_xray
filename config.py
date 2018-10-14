accb387a69e3cce2d82a1a87a45b95c03cec246d
varkristof <37442777+varkristof@users.noreply.github.com>
2018-10-14 16:13:08 +0200

    Added the config file

---
+++ config.py
@@ -0,0 +1,19 @@
+
+#The downloader script going to download the (compressed) dataset into this folder
+DATASET_PATH = "E:\chest_dataset\\teszt"
+
+#The unzipped dataset is in this folder
+EXT_DATASET_PATH = DATASET_PATH + "\images"
+
+#Path and name of the compressed dataset
+FILENAME = DATASET_PATH + "\data.zip"
+
+#Size of the images in px
+#IMG_SIZE x IMG_SIZE
+IMG_SIZE = 256
+
+#Rate of the train-, validation- and testsets
+#The sum of them have to be one
+TRAIN_RATE = 0.5
+VALID_RATE = 0.2
+TEST_RATE = 0.3
