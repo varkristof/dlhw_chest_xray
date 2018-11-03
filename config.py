88822d4dab87c21dd052d749dccdff40f5b63341
Bianka Rácz <rczbianka1@gmail.com>
2018-11-03 22:40:10 +0100

    Config modified

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 DATASET_PATH = "res"
 
 #The unzipped dataset is in this folder
-EXT_DATASET_PATH = DATASET_PATH + "\images"
+EXT_DATASET_PATH = DATASET_PATH + "/images"
 
 #Path and name of the compressed dataset
 FILENAME = DATASET_PATH + "\data.zip"
